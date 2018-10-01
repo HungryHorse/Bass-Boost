b2447c5256323676cd269cddf931a44b62bfeeca
HungryHorse <33032163+HungryHorse@users.noreply.github.com>
2018-10-01 11:32:53 +0100

    Commit 1 - Started creating movement, added camera folllow

---
+++ Bass Boost/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+    public Transform target;
+    public float smoothing;
+    Vector3 adjustVector = new Vector3(0, 15, -10);
+    	
+	// Update is called once per frame
+	void Update () {
+
+        transform.position = Vector3.Lerp(gameObject.transform.position, target.position + adjustVector, 0.01f * smoothing);
+		
+	}
+}
