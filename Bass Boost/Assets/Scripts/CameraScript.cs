7d3b6a68e513e8ba6ac38da7eb042dd4052676a1
HungryHorse <33032163+HungryHorse@users.noreply.github.com>
2018-10-05 15:57:33 +0100

    Movement changes

--- Bass Boost/Assets/Scripts/CameraScript.cs
+++ Bass Boost/Assets/Scripts/CameraScript.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class CameraScript : MonoBehaviour {
     public Transform target;
     public float smoothing;
-    Vector3 adjustVector = new Vector3(0, 15, -10);
-    	
-	// Update is called once per frame
-	void Update () {
+    public float x, y, z;
+    Vector3 adjustVector;
+
+    private void Start()
+    {
+        adjustVector = new Vector3(x, y, z);
+    }
+
+    // Update is called once per frame
+    void LateUpdate () {
 
         transform.position = Vector3.Lerp(gameObject.transform.position, target.position + adjustVector, 0.01f * smoothing);
 		
--- Bass Boost/Assets/Scripts/PlayerMovement.cs
+++ Bass Boost/Assets/Scripts/PlayerMovement.cs
@@ -5,29 +5,47 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
 
     public float Speed = 5f;
+    public float maxCharge = 5f;
+    public float chargeRate = 0.25f;
+    public float minCharge = 2f;
 
+    private bool boost = false;
+    private float charge;
     private Rigidbody player;
     private Vector3 facing;
+    private Vector3 DashForce;
+
 
     void Start()
     {
         player = GetComponent<Rigidbody>();
+        charge = minCharge;
     }
 
     void Update()
     {
+        
         facing = Vector3.zero;
         facing.x = Input.GetAxis("Horizontal");
-        Debug.Log(Input.GetAxis("Horizontal"));
         facing.z = Input.GetAxis("Vertical");
         if (facing != Vector3.zero)
         {
-            transform.forward = facing;
+            facing = new Vector3(facing.x, 0, facing.z);
+        }
+        
+        if (Input.GetButton("Jump"))
+        {
+            facing /= 3;
+            if(charge <= maxCharge)
+            {
+                charge += Time.deltaTime * chargeRate;
+            }
         }
 
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonUp("Jump"))
         {
-            facing *= 2;
+            boost = true;
+            charge = minCharge;
         }
 
     }
@@ -35,6 +53,16 @@ public class PlayerMovement : MonoBehaviour {
 
     void FixedUpdate()
     {
-        player.MovePosition(player.position + facing * Speed * Time.fixedDeltaTime);
+        
+        if (boost == true)
+        {
+            Debug.Log("Boost");
+            player.AddForce( * charge * 10);
+            boost = false;
+        }
+        else
+        {
+            player.AddForce(facing * Speed);
+        }
     }
 }
